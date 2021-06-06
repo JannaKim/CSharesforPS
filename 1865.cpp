490c10732889c3a6b1cf87370aa66bb8811c6615
Yuzuyuz <84283540+Yuzuyuz@users.noreply.github.com>
2021-06-06 22:30:48 +0900

    1865: 웜홀
    https://www.acmicpc.net/problem/1865

---
+++ 1865.cpp
@@ -0,0 +1,72 @@
+#include <deque>
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+const int inf = 1000000000;
+
+int t, n, m, w;
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> t;
+	
+	while (t--)
+	{
+		int flag = 0;
+		int d[501];
+		fill(d, d + 1, inf);
+		d[0] = 0;
+		vector <pair<int, int>> path[501];
+		cin >> n >> m >> w;
+		for (int i = 0; i < m; i++)
+		{
+			int s, e, t;
+			cin >> s >> e >> t;
+			path[s].push_back(make_pair(e, t));
+			path[e].push_back(make_pair(s, t));
+		}
+		for (int i = 0; i < w; i++)
+		{
+			int s, e, t;
+			cin >> s >> e >> t;
+			path[s].push_back(make_pair(e, -t));
+		}
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				for (auto& a : path[j])
+				{
+					int next = a.first;
+					int adv = a.second;
+					if (i < n - 1)
+					{
+						if (d[j] != inf && d[j] + adv < d[next])
+						{
+							d[next] = d[j] + adv;
+						}
+					}
+					else
+					{
+						if (d[j] != inf && d[j] + adv < d[next])
+						{
+							flag = 1;
+						}
+					}
+				}
+			}
+		}
+		if (flag == 1)
+		{
+			cout << "YES\n";
+			continue;
+		}
+		cout << "NO\n";
+	}
+}
